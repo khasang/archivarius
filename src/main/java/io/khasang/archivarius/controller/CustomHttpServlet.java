2c3c0e08c21fc4dc1eb97bc48d8cd934bd4930fb
yuri <dynamost@dynamost.ru>
2016-11-06 23:44:14 +0300

    Add update salary method for employees

---
+++ src/main/java/io/khasang/archivarius/controller/CustomHttpServlet.java
@@ -0,0 +1,7 @@
+package io.khasang.archivarius.controller;
+
+/**
+ * Created by yuri on 05.11.16.
+ */
+public class CustomHttpServlet {
+}
---
+++ src/main/java/io/khasang/archivarius/model/QueryExample.java
@@ -0,0 +1,7 @@
+package io.khasang.archivarius.model;
+
+/**
+ * Created by yuri on 06.11.16.
+ */
+public class QueryExample {
+}
