650bae8b11bca6a6198f120d48a03e16a88795ef
Kuznetsov Sergey <azon.sk@gmail.com>
2016-11-07 22:14:35 +0300

    add spring security with db auth

--- src/main/java/io/khasang/archivarius/controller/AppController.java
+++ src/main/java/io/khasang/archivarius/controller/AppController.java
@@ -3,9 +3,13 @@ package io.khasang.archivarius.controller;
 import io.khasang.archivarius.model.Message;
 import io.khasang.archivarius.model.QueryExample;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class AppController {
@@ -30,5 +34,19 @@ public class AppController {
     public String secure(Model model){
         model.addAttribute("secure", "This is very secure page");
         return "secure";
+    }
+
+    @RequestMapping("/user/api")
+    public String userApi(Model model){
+        model.addAttribute("api", "ты не сможешь сюда попасть");
+        return "api";
+    }
+
+    @RequestMapping(value = {"hello/{name}"}, method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("encode");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(name));
+        return modelAndView;
     }
 }
