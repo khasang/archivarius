dc40e381f3ed9171f7457b20b63e4e74a5292165
sliver449 <sliver449@gmail.com>
2016-11-16 17:17:16 +0300

    add Deleting table

--- src/main/java/io/khasang/archivarius/controller/AppController.java
+++ src/main/java/io/khasang/archivarius/controller/AppController.java
@@ -16,4 +16,57 @@ public class AppController {
         model.addAttribute("hello", message.getHelloMessage());
         return "hello";
     }
+<<<<<<< Updated upstream
+=======
+
+    @RequestMapping("/create")
+    public String create(Model model) {
+        model.addAttribute("create", queryExample.tableCreation());
+        return "create";
+    }
+
+    @RequestMapping("/update")
+    public String update(Model model) {
+        model.addAttribute("update", queryExample.tableUpdate());
+        return "update";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(Model model) {
+        model.addAttribute("delete", queryExample.tableDelete());
+        return "delete";
+    }
+    
+    @RequestMapping("/insert")
+    public String insert(Model model) {
+        model.addAttribute("insert", queryExample.tableInsert(42, "Vasya Pupkin", 33, "Moscow, Kremlin", 100500));
+        return "insert";
+    }
+    
+    @RequestMapping("/admin/page")
+    public String secure(Model model) {
+        model.addAttribute("secure", "This is very secure page");
+        return "secure";
+    }
+
+    @RequestMapping("/user/api")
+    public String userApi(Model model) {
+        model.addAttribute("api", "ты не сможешь сюда попасть");
+        return "api";
+    }
+
+    @RequestMapping(value = {"hello/{name}"}, method = RequestMethod.GET)
+    public ModelAndView hello(@PathVariable("name") String name) {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("encode");
+        modelAndView.addObject("crypt", new BCryptPasswordEncoder().encode(name));
+        return modelAndView;
+    }
+
+    @RequestMapping("/backup")
+    public String backup(Model model) {
+        model.addAttribute("backup", databaseBackup.backup());
+        return "backup";
+    }
+>>>>>>> Stashed changes
 }
---
+++ web/WEB-INF/views/delete.jsp
@@ -0,0 +1,10 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Delete table page</title>
+</head>
+<body>
+<h1>Table deleting status is: </h1>
+<p>${delete}</p>
+</body>
+</html>
