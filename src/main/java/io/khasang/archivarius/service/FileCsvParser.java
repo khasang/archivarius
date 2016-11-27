ee50ffd7a820a464d94826c92a7a466e3fa5d37d
tianaV <v.tianav2@gmail.com>
2016-11-27 12:18:21 +0300

    add testFilesExist, add testContentsFile, public class FileCsvParser ( public boolean checkFiles)

---
+++ src/main/java/io/khasang/archivarius/service/FileCsvParser.java
@@ -0,0 +1,117 @@
+package io.khasang.archivarius.service;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.stereotype.Component;
+
+import java.io.File;
+import java.io.IOException;
+
+/**
+ * Tanya on 27.11.2016.
+ */
+@Component
+public class FileCsvParser {
+    public File getFile1() {
+        return file1;
+    }
+
+    public void setFile1(File file1) {
+        this.file1 = file1;
+    }
+
+    public File getFile2() {
+        return file2;
+    }
+
+    public void setFile2(File file2) {
+        this.file2 = file2;
+    }
+
+    public File getFile3() {
+        return file3;
+    }
+
+    public void setFile3(File file3) {
+        this.file3 = file3;
+    }
+
+    public File getFile4() {
+        return file4;
+    }
+
+    public void setFile4(File file4) {
+        this.file4 = file4;
+    }
+
+    public File getFile5() {
+        return file5;
+    }
+
+    public void setFile5(File file5) {
+        this.file5 = file5;
+    }
+
+    public File getFile6() {
+        return file6;
+    }
+
+    public void setFile6(File file6) {
+        this.file6 = file6;
+    }
+
+    public File getFile7() {
+        return file7;
+    }
+
+    public void setFile7(File file7) {
+        this.file7 = file7;
+    }
+
+    public File getFile8() {
+        return file8;
+    }
+
+    public void setFile8(File file8) {
+        this.file8 = file8;
+    }
+
+    public File getFile9() {
+        return file9;
+    }
+
+    public void setFile9(File file9) {
+        this.file9 = file9;
+    }
+
+    public File getFile10() {
+        return file10;
+    }
+
+    public void setFile10(File file10) {
+        this.file10 = file10;
+    }
+
+    private File file1 = new ClassPathResource("examples/source001.csv").getFile();
+    private File file2 = new ClassPathResource("examples/source002.csv").getFile();
+    private File file3 = new ClassPathResource("examples/source003.csv").getFile();
+    private File file4 = new ClassPathResource("examples/source004.csv").getFile();
+    private File file5 = new ClassPathResource("examples/source005.csv").getFile();
+    private File file6 = new ClassPathResource("examples/source006.csv").getFile();
+    private File file7 = new ClassPathResource("examples/source007.csv").getFile();
+    private File file8 = new ClassPathResource("examples/source008.csv").getFile();
+    private File file9 = new ClassPathResource("examples/source009.csv").getFile();
+    private File file10 = new ClassPathResource("examples/source010.csv").getFile();
+
+    public FileCsvParser() throws IOException {
+    };
+
+    public boolean checkFiles(File file) {
+        if (file.exists()) {
+            return true;
+        } else {
+            System.err.println("This file does not exist");
+            return false;
+        }
+    }
+}
--- src/test/java/io/khasang/archivarius/service/FileCsvParserTest.java
+++ src/test/java/io/khasang/archivarius/service/FileCsvParserTest.java
@@ -1,22 +1,58 @@
 package io.khasang.archivarius.service;
 
+import io.khasang.archivarius.config.AppConfig;
+import io.khasang.archivarius.config.HibernateConfig;
+import io.khasang.archivarius.config.application.WebConfig;
 import org.junit.Assert;
 import org.junit.Ignore;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+@ContextConfiguration(classes = {AppConfig.class, WebConfig.class, HibernateConfig.class})
 
 public class FileCsvParserTest {
+    @Autowired
+    FileCsvParser fileCsvParser;
     @Ignore
     @Test
     public void testEntityIsAlive(){
 //        Assert.assertNotNull(new Report());
     }
 
-    @Ignore
     @Test
     public void testFilesExist(){
-//        Assert.assertEquals(true, fileCheckService.checkFiles());
-    }
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile1()));
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile2()));
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile3()));
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile4()));
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile5()));
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile6()));
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile7()));
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile8()));
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile9()));
+        Assert.assertEquals(true, fileCsvParser.checkFiles(fileCsvParser.getFile10()));
+            }
 
+    @Test
+    public void testContentsFile() {
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile1()));
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile2()));
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile3()));
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile4()));
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile5()));
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile6()));
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile7()));
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile8()));
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile9()));
+        Assert.assertEquals(true, fileCsvParser.countNumderRows(fileCsvParser.getFile10()));
+
+    }
     @Ignore
     @Test
     public void testSomeOneAtVk(){
