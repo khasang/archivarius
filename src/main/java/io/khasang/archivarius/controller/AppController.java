309287446a01b61c2f0d43fb16a5037028702c4b
yuri <dynamost@dynamost.ru>
2016-12-09 02:54:32 +0300

    add login form

--- src/main/java/io/khasang/archivarius/controller/AppController.java
+++ src/main/java/io/khasang/archivarius/controller/AppController.java
@@ -130,4 +130,9 @@ public class AppController {
         return "vklist";
 
     }
+
+    @RequestMapping(value = "/login", method = RequestMethod.GET)
+    public String loginPage() {
+        return "login";
+    }
 }
