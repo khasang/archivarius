79b7d5cc7d6c61e690e1380a482f5e4dfc97872f
yuri <dynamost@dynamost.ru>
2016-12-19 04:48:46 +0300

    cut fragments

--- src/main/java/io/khasang/archivarius/controller/AppController.java
+++ src/main/java/io/khasang/archivarius/controller/AppController.java
@@ -10,6 +10,7 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -27,7 +28,7 @@ public class AppController {
     @Autowired
     ReportService reportService;
 
-    @RequestMapping(value = "/", method = RequestMethod.GET)
+    @GetMapping(value = "/")
     public String index(Model model) {
         return "index";
     }
