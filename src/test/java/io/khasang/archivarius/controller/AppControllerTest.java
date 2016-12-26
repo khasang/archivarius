4273add714976bab3696c951320b32933d552349
yuri <dynamost@dynamost.ru>
2016-12-26 23:31:31 +0300

    ad appcontroller test

--- src/test/java/io/khasang/archivarius/controller/AppControllerTest.java
+++ src/test/java/io/khasang/archivarius/controller/AppControllerTest.java
@@ -1,4 +1,48 @@
 package io.khasang.archivarius.controller;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.MockitoJUnitRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+@RunWith(MockitoJUnitRunner.class)
 public class AppControllerTest {
+    private MockMvc mockMvc;
+
+    @InjectMocks
+    AppController controller;
+
+    @Before
+    public void setup() {
+        MockitoAnnotations.initMocks(this);
+        mockMvc = standaloneSetup(controller)
+                .setViewResolvers(viewResolver())
+                .build();
+    }
+
+    private InternalResourceViewResolver viewResolver() {
+        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+        viewResolver.setPrefix("/WEB-INF/templates/");
+        viewResolver.setSuffix(".html");
+        return viewResolver;
+    }
+
+    @Test
+    public void homeTest() throws Exception {
+        mockMvc.perform(get("/inbox"))
+                .andDo(print())
+                .andExpect(status().isOk())
+        .andExpect(content().string(containsString("</head>")));
+    }
 }
