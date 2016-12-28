3784c392b3e82bffcb5748031830dcab50188e02
NeocortexF <eugeneshamkin@mail.ru>
2016-12-28 16:46:09 +0500

    Merging of contollers

--- src/main/java/io/khasang/archivarius/controller/DocumentController.java
+++ src/main/java/io/khasang/archivarius/controller/DocumentController.java
@@ -13,6 +13,16 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 @Controller
 @RequestMapping("/document")
 public class DocumentController {
@@ -46,10 +56,50 @@ public class DocumentController {
     }
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String showDocumentForm(ModelMap model) {
+    public @ResponseBody
+    String showDocumentForm(ModelMap model, @RequestParam("file") MultipartFile file) {
         model.addAttribute("doctypes", docTypeService.getDocTypeList());
         model.addAttribute("document", new Document());
         return "forms/document";
+    }
+
+    /**
+     * Upload single file using Spring Controller
+     */
+    @RequestMapping(value = "/document/uploadFile", method = RequestMethod.POST)
+    public @ResponseBody
+    String uploadFileHandler(@RequestParam("name") String name,
+                             @RequestParam("file") MultipartFile file) {
+
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+
+                // Creating the directory to store file
+                String rootPath = System.getProperty("catalina.home");
+                File dir = new File(rootPath + File.separator + "tmpFiles");
+                if (!dir.exists())
+                    dir.mkdirs();
+
+                // Create the file on server
+                File serverFile = new File(dir.getAbsolutePath()
+                        + File.separator + name);
+                BufferedOutputStream stream = new BufferedOutputStream(
+                        new FileOutputStream(serverFile));
+                stream.write(bytes);
+                stream.close();
+
+                log.info("Server File Location="
+                        + serverFile.getAbsolutePath());
+
+                return "You successfully uploaded file=" + name;
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name
+                    + " because the file was empty.";
+        }
     }
 
     @RequestMapping(value = "/", method = RequestMethod.POST)
