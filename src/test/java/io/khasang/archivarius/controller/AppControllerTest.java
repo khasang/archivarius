b47904e62eb50113c800b052fef3b81c33617d3c
yuri <dynamost@dynamost.ru>
2016-12-30 08:10:11 +0300

    add test to appController

--- src/test/java/io/khasang/archivarius/controller/AppControllerTest.java
+++ src/test/java/io/khasang/archivarius/controller/AppControllerTest.java
@@ -1,20 +1,16 @@
 package io.khasang.archivarius.controller;
 
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
-import org.mockito.MockitoAnnotations;
 import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import static org.hamcrest.CoreMatchers.containsString;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
 
 @RunWith(MockitoJUnitRunner.class)
 public class AppControllerTest {
@@ -23,20 +19,6 @@ public class AppControllerTest {
     @InjectMocks
     AppController controller;
 
-    @Before
-    public void setup() {
-        MockitoAnnotations.initMocks(this);
-        mockMvc = standaloneSetup(controller)
-                .setViewResolvers(viewResolver())
-                .build();
-    }
-
-    private InternalResourceViewResolver viewResolver() {
-        InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-        viewResolver.setPrefix("/WEB-INF/templates/");
-        viewResolver.setSuffix(".html");
-        return viewResolver;
-    }
 
     @Test
     public void homeTest() throws Exception {
