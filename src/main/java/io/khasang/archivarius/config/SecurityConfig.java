8e230d07e5d270cc4dbd2754673d487af5d9c7a6
yuri <dynamost@dynamost.ru>
2017-01-07 00:57:28 +0300

    hello user message in navigation bar

--- src/main/java/io/khasang/archivarius/config/SecurityConfig.java
+++ src/main/java/io/khasang/archivarius/config/SecurityConfig.java
@@ -34,6 +34,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                     .antMatchers("/css/**").permitAll()
+                    .antMatchers("/images/**").permitAll()
                     .antMatchers("/hello/**").permitAll()
                     .anyRequest().authenticated()
                 .and()
