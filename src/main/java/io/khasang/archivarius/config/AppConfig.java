34500a3894153b03d9aa1ac452a4cff07218a965
yuri <dynamost@dynamost.ru>
2017-02-09 22:47:44 +0300

    add starting initialising

--- src/main/java/io/khasang/archivarius/config/AppConfig.java
+++ src/main/java/io/khasang/archivarius/config/AppConfig.java
@@ -1,9 +1,12 @@
 package io.khasang.archivarius.config;
 
+import io.khasang.archivarius.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.context.event.EventListener;
 import org.springframework.core.env.Environment;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -16,6 +19,8 @@ import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 public class AppConfig {
     @Autowired
     Environment environment;
+    @Autowired
+    UserRepository userRepository;
 
     @Bean
     public UserDetailsService userDetailsService() {
@@ -41,5 +46,16 @@ public class AppConfig {
         JdbcTemplate jdbcTemplate = new JdbcTemplate();
         jdbcTemplate.setDataSource(dataSource());
         return jdbcTemplate;
+    }
+
+    @EventListener(ContextRefreshedEvent.class)
+    public void contextRefreshedEvent() {
+        if (userRepository.findAll().isEmpty()) {
+            JdbcTemplate jdbcTemplate = jdbcTemplate();
+            jdbcTemplate.execute("INSERT INTO worker(id, companyworker, lastname) VALUES (1, TRUE, 'admin')");
+            jdbcTemplate.execute("INSERT INTO users(id, login, password, worker_id) VALUES (1, 'admin', '$2a$10$FB3WSArYXyhy94xELFE9f.45/nHD6lnww5PGI0MCMikWQUDofHKuW', 1)");
+            jdbcTemplate.execute("INSERT INTO roles(id, role_name) VALUES (1, 'ROLE_ADMIN'), (2, 'ROLE_USER'), (3, 'ROLE_DOCUMENTOVED')");
+            jdbcTemplate.execute("INSERT INTO users_roles(users_id, roles_id) VALUES (1, 1)");
+        }
     }
 }
